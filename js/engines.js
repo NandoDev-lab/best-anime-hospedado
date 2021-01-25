c176c357d83014dd9815fc9b9b7352c8c53a8d50
FERNANDO LUIS DE OLIVEIRA SALDANHA <75080506+NandoDev-lab@users.noreply.github.com>
2021-01-25 17:20:09 -0300

    Add files via upload

---
+++ js/engines.js
@@ -0,0 +1,27 @@
+
+function piscar(){
+    if (document.getElementById("shen").checked) {
+        document.getElementById("piscante").innerText='Faça sua escolha';
+        document.getElementById("piscante").style.marginLeft='120px';
+        document.getElementById("piscante").style.animationPlayState='running';
+        document.getElementById("shenlong").style.animationPlayState="running";
+        document.getElementById("shenlong").style.animationIterationCount="1";
+      
+    }else {
+        document.getElementById("piscante").innerText='Clique para fazer o pedido';
+        document.getElementById("piscante").style.marginLeft='80px';
+        document.getElementById("piscante").style.display="inherit";
+        document.getElementById("piscante").style.animationPlayState='running';
+        document.getElementById("shenlong").style.animationPlayState="running";
+        document.getElementById("shenlong").style.animationIterationCount="infinite";
+    }
+    
+};
+
+function ampliarConteudo(){
+    var assistindo=document.getElementsByClassName("embed-responsive");
+    var texto=document.getElementById("mainframe").innerText;
+    if (assistindo.onclick){
+        document.texto.innerText="Assistindo Agora";
+    }
+}
