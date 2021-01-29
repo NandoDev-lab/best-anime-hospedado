58a658a354882ec2a73c95c506f9b10e9ad64e3e
FERNANDO LUIS DE OLIVEIRA SALDANHA <nandomusic1@gmail.com>
2021-01-29 09:39:27 -0300

    menu
    mudanças css no fundo do menu

--- js/engines.js
+++ js/engines.js
@@ -2,10 +2,12 @@
 function piscar(){
     if (document.getElementById("shen").checked) {
         document.getElementById("piscante").innerText='Faça sua escolha';
+        document.getElementById("piscante").style.zIndex="4";
         document.getElementById("piscante").style.marginLeft='120px';
         document.getElementById("piscante").style.animationPlayState='running';
         document.getElementById("shenlong").style.animationPlayState="running";
         document.getElementById("shenlong").style.animationIterationCount="1";
+    
       
     }else {
         document.getElementById("piscante").innerText='Clique para fazer o pedido';
